6f70c72a32e25e7d0520ce31a0fdb0841ff2071b
Callum <Callum.moore@microfocus.com>
2023-07-19 11:16:49 +0100

    Adding the command shortcut for NvimTreeToogle

--- init.lua
+++ init.lua
@@ -530,3 +530,13 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true}
+  if opts then
+    options = vim.tbl_extend('force', options,opts)
+  end
+  vim.api.nvim_set_keymap(mode,lhs,rhs,options)
+end
+
+map('', '<leader>tt', ':NvimTreeToggle')
